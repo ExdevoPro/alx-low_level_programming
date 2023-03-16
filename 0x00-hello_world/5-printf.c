8c6e1b4268edef1033015379308804dac0f34f43
ExdevoPro <koechobadia97@gmail.com>
2023-03-16 10:03:44 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  this is a valid main function
+ *
+ * Return: It always return zero (0) when compilation is successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
